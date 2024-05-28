267577b1b88523bef2796a5056faf684d76e7834
SUsmna <159513831+SUsmna@users.noreply.github.com>
2024-05-28 20:06:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -24,10 +24,14 @@ class Square extends Rectangle {
 		return 4 * this._side;
 	}
 }
-const rectangle = new Rectangle();
+const rectangle = new Rectangle(5, 10);
+console.log(rectangle.width);
+console.log(rectangle.height);
 console.log(rectangle.getArea());
 
-const square = new Square(7);
+const square = new Square();
+console.log(Square.width);
+console.log(Square.height);
 console.log(square.getPerimeter());
 
 // Do not change the code below this line
